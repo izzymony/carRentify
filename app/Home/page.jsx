8c9ceb5f53f75b590e079a9cc2cb4fbea1410717
izzymony <ojehonmonisrael008@gmail.com>
2025-06-11 00:13:43 +0100

    omo

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -24,7 +24,7 @@ const page =() =>{
   }, [searchTerm]);
 
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className="">
       <Nav />
       <div>
         <header className="relative">
